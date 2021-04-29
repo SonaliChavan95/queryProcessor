b9aee6b0f541a7487fc5e39145285b84912de355
SonaliChavan95 <schava1@stevens.edu>
2021-04-29 12:58:09 -0400

    Read query file

--- src/qp/MfStructure.java
+++ src/qp/MfStructure.java
@@ -20,8 +20,8 @@ public class MfStructure {
 	public ArrayList<String> F;
 	// Select condition Vector
 	public ArrayList<String> CV;
-	// Having Conditions
-	public ArrayList<String> G;
+	// Having Condition
+	public String G;
 	
 	
 	MfStructure() {
@@ -29,7 +29,7 @@ public class MfStructure {
 		this.V = new ArrayList<String>();
 		this.F = new ArrayList<String>();
 		this.CV = new ArrayList<String>();
-		this.G = new ArrayList<String>();
+		this.G = new String();
 		this.n = 0;
 	}
 	
@@ -49,10 +49,11 @@ public class MfStructure {
 			if (myObj.exists()) {
 				Scanner myReader = new Scanner(myObj);;
 
-				String input;
+				String input = "";
 				while (myReader.hasNextLine()) {
-					input = myReader.nextLine();
-					 System.out.println(input);
+					if(!input.equals("HAVING_CONDITION(G):"))
+						input = myReader.nextLine();
+					System.out.println(input);
 
 					switch(input) {
 					case "SELECT ATTRIBUTE(S):":
@@ -65,14 +66,48 @@ public class MfStructure {
 						}
 						break;
 					case "NUMBER OF GROUPING VARIABLES(n):":
+						if(myReader.hasNextLine()) {
+							input = myReader.nextLine();
+							this.n = Integer.parseInt(input);
+							// System.out.println(this.S.get(1));
+						}
 						break;
 					case "GROUPING ATTRIBUTES(V):":
+						if(myReader.hasNextLine()) {
+							input = myReader.nextLine();
+							String arr[] = input.split("[,]"); 
+							for(String i : arr)
+								this.V.add(i.trim());
+							// System.out.println(this.V.get(1));
+						}
 						break;
 					case "F-VECT([F]):":
+						if(myReader.hasNextLine()) {
+							input = myReader.nextLine();
+							String arr[] = input.split("[,]"); 
+							for(String i : arr)
+								this.F.add(i.trim());
+							// System.out.println(this.F.get(1));
+						}
 						break;
 					case "SELECT CONDITION-VECT([σ]):":
+						while(myReader.hasNextLine()) {
+							input = myReader.nextLine();
+							if(input.equals("HAVING_CONDITION(G):"))
+								break;
+							
+							System.out.println(input);
+							this.CV.add(input.trim());								
+						}
+						// System.out.println(this.CV.get(3));
+
 						break;
 					case "HAVING_CONDITION(G):":
+						if(myReader.hasNextLine()) {
+							input = myReader.nextLine();
+							this.G = input.trim();
+							System.out.println(this.G);
+						}
 						break;
 					}
 				}
@@ -86,8 +121,6 @@ public class MfStructure {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 	
 	
